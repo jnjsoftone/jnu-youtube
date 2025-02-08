34b8cc08bd0d930f0a6c15217775dff3e0ed9912
JnJ One <jnjsoft.one@gmail.com>
2025-02-09 08:07:41 +0900

    publish

--- publish.bat
+++ publish.bat
@@ -1,34 +1,62 @@
 @echo off
-REM [syntax] publish patch|minor|major
-REM default: patch
+REM [syntax] publish [patch|minor|major] [-m "commit message"]
+REM default: patch, "chore: build for publish"
 
-IF "%~1"=="" (
-  SET mode=patch
-) ELSE (
-  SET mode=%1
+SET mode=patch
+SET commit_msg=chore: build for publish
+
+:parse_args
+IF "%~1"=="" GOTO end_parse
+IF "%~1"=="-m" (
+  SET commit_msg=%~2
+  SHIFT
+  SHIFT
+  GOTO parse_args
 )
+IF "%~1"=="patch" (
+  SET mode=patch
+  SHIFT
+  GOTO parse_args
+)
+IF "%~1"=="minor" (
+  SET mode=minor
+  SHIFT
+  GOTO parse_args
+)
+IF "%~1"=="major" (
+  SET mode=major
+  SHIFT
+  GOTO parse_args
+)
+SHIFT
+GOTO parse_args
+:end_parse
 
-REM 1. 빌드
-call yarn clean:win
-if errorlevel 1 goto :error
-call yarn build
+REM 1. git pull 먼저 실행하여 원격 변경사항 가져오기
+git pull
 if errorlevel 1 goto :error
 
-REM 2. git 변경사항 커밋
+REM 2. 빌드
+@REM call yarn clean:win
+@REM if errorlevel 1 goto :error
+call npm run build
+if errorlevel 1 goto :error
+
+REM 3. git 변경사항 커밋
 git add .
 if errorlevel 1 goto :error
-git commit -m "chore: build for publish"
+git commit -m "%commit_msg%"
 if errorlevel 1 goto :error
 
-REM 3. npm 버전 업데이트 (이때 자동으로 버전 태그가 생성됨)
+REM 4. npm 버전 업데이트 (이때 자동으로 버전 태그가 생성됨)
 call npm version %mode%
 if errorlevel 1 goto :error
 
-REM 4. git push
+REM 5. git push
 git push --follow-tags
 if errorlevel 1 goto :error
 
-REM 5. npm 배포
+REM 6. npm 배포
 call npm publish
 if errorlevel 1 goto :error
 
--- publish.sh
+++ publish.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
-# [syntax] ./publish.sh patch|minor|major
-# default: patch
+# [syntax] ./publish.sh [patch|minor|major] [-m "commit message"]
+# default: patch, "chore: build for publish"
 
-mode=${1:-patch}
+mode="patch"
+commit_msg="chore: build for publish"
 
-# 1. 빌드
-yarn clean:mac && yarn build && \
-# 2. git 변경사항 커밋
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -m)
+      commit_msg="$2"
+      shift 2
+      ;;
+    patch|minor|major)
+      mode="$1"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+# 1. git pull 먼저 실행하여 원격 변경사항 가져오기
+git pull && \
+# 2. 빌드
+npm run build && \
+# 3. git 변경사항 커밋
 git add . && \
-git commit -m "chore: build for publish" && \
-# 3. npm 버전 업데이트 (이때 자동으로 버전 태그가 생성됨)
+git commit -m "$commit_msg" && \
+# 4. npm 버전 업데이트 (이때 자동으로 버전 태그가 생성됨)
 npm version $mode && \
-# 4. git push
+# 5. git push
 git push --follow-tags && \
-# 5. npm 배포
+# 6. npm 배포
 npm publish
